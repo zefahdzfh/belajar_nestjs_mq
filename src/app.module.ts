04e6460c049d2834d23ee2f1e9a75ce07336dbdf
zefahdzfh <zefakalibata@gmail.com>
2023-11-07 10:51:05 +0700

    added mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { Pts2023Service } from './pts-2023/pts-2023.service';
 import { Pts2023Controller } from './pts-2023/pts-2023.controller';
 import { Pts2023Module } from './pts-2023/pts-2023.module';
 import { AuthModule } from './app/auth/auth.module';
+import { MailModule } from './app/mail/mail.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AuthModule } from './app/auth/auth.module';
     BookModule,
     Pts2023Module,
     AuthModule,
+    MailModule,
   ],
   controllers: [AppController, Pts2023Controller],
   providers: [AppService, Pts2023Service],
