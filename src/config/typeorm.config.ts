767e6d245a7d4845a8feaa5887ca9e0c179c660e
zefahdzfh <zefakalibata@gmail.com>
2024-01-23 10:23:12 +0700

    ganti database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   port: 3306, //port default 3306 lihat xampp
   username: 'root', // username default xampp root
   password: '', // password default xampp string kosong
-  database: 'belajar_projek-xi',
+  database: 'belajar-xi',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: true,
